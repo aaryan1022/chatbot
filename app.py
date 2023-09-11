3347e520c375e27165d0ba7d0b3eb6b67f6a76a4
aaryan1022 <104364223+aaryan1022@users.noreply.github.com>
2023-09-11 20:44:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ from langchain.schema import SystemMessage
 from fastapi import FastAPI
 
 load_dotenv()
-brwoserless_api_key = os.getenv("BROWSERLESS_API_KEY")
-serper_api_key = os.getenv("SERP_API_KEY")
+brwoserless_api_key = os.getenv("B_API_KEY")
+serper_api_key = os.getenv("S_API_KEY")
 
 # 1. Tool for search
 
@@ -144,12 +144,13 @@ system_message = SystemMessage(
             you do not make things up, you will try as hard as possible to gather facts & data to back up the research
             
             Please make sure you complete the objective above with the following rules:
-            1/ You should do enough research to gather as much information as possible about the objective
-            2/ If there are url of relevant links & articles, you will scrape it to gather more information
-            3/ After scraping & search, you should think "is there any new things i should search & scraping based on the data I collected to increase research quality?" If answer is yes, continue; But don't do this more than 3 iteratins
-            4/ You should not make things up, you should only write facts & data that you have gathered
-            5/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research
-            6/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research"""
+            1/ Provide information specifically relevant to the online courses at QAI Global Institute. Only the link: 'https://qaiglobalinstitute.com/', including all of its sublinks.
+            2/ You should do enough research to gather as much information as possible about the objective
+            3/ If there are url of relevant sublinks & articles on the qai global website, you will scrape it to gather more information
+            4/ After scraping & search, you should think "is there any part of the qai global website search & scraping based on the data I collected to increase research quality?" If answer is yes, continue; But don't do this more than 3 iteratins
+            5/ You should not make things up, you should only write facts & data that you have gathered
+            6/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research
+            7/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research"""
 )
 
 agent_kwargs = {
